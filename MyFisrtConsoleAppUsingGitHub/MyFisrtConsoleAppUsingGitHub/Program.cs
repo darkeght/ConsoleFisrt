b7ce58c2205f45b7c04bb94be7096ac6a84939a4
Felipe Machado <darkeght@gmail.com>
2019-07-09 00:57:33 -0300

    Alter font

--- MyFisrtConsoleAppUsingGitHub/MyFisrtConsoleAppUsingGitHub/Program.cs
+++ MyFisrtConsoleAppUsingGitHub/MyFisrtConsoleAppUsingGitHub/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace MyFisrtConsoleAppUsingGitHub
 {
@@ -7,7 +8,34 @@ namespace MyFisrtConsoleAppUsingGitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Alter Text For Example!");
+
+            Teste teste = new Teste();
+
+            Console.WriteLine($"{teste?.Pointer}");
+
+            var returnFindPrime = PrimeNumber(9999999);
+
             Console.ReadKey();
         }
+
+        public class Teste
+        {
+            public int? Pointer { get; set; } = null;
+        }
+
+        static System.Collections.Generic.List<int> PrimeNumber(long num)
+        {
+            var resultPrime  = Enumerable.Range(0,(int)(Math.Floor(2.52 * Math.Sqrt(num) / Math.Log(num)))).Aggregate(
+            Enumerable.Range(2,(int)(num - 1.0)).ToList(),
+            (result, index) => {
+                var bp = result[index]; var sqr = bp * bp;
+                result.RemoveAll(i => i >= sqr && i % bp == 0);
+                return result;
+            }
+        );
+
+            return resultPrime;
+        }
+
     }
 }
