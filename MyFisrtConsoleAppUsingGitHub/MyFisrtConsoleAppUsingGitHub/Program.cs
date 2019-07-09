26c255d03aeffd9b3a39e88f6b1d0f58163da472
Felipe Machado <darkeght@gmail.com>
2019-07-08 23:43:43 -0300

    Fisrt Commit Console App

---
+++ MyFisrtConsoleAppUsingGitHub/MyFisrtConsoleAppUsingGitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyFisrtConsoleAppUsingGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Example For Git");
+            Console.ReadKey();
+        }
+    }
+}
